7f553a8f1dd0bd7d3611f1182dc0365f1979e664
David Klein <david.klein@tu-braunschweig.de>
2020-03-21 16:02:06 +0100

    Models, models, models!

--- models/coordinates.py
+++ models/coordinates.py
@@ -1,4 +1,4 @@
-class Coordinate:
+class Coordinates:
     def __init__(self, latitude, longitude):
         self._latitude = latitude
         self._longitude = longitude
@@ -12,6 +12,6 @@ class Coordinate:
         return self._longitude
 
     def __repr__(self):
-        return ('Coordinate(latitude={self._latitude!r}, '
+        return ('Coordinates(latitude={self._latitude!r}, '
                 'longitude={self._longitude!r})'
                 ).format(self=self)
---
+++ models/pharmacy.py
@@ -0,0 +1,30 @@
+class Coordinate:
+    def __init__(self, id, name, address, coordinates):
+        self._id = id
+        self._name = name
+        self._address = address
+        self._coordinates = coordinates
+
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def address(self):
+        return self._address
+
+    @property
+    def coordinates(self):
+        return self._coordinates
+
+    def __repr__(self):
+        return ('Pharmacy(id={self._id!r}, '
+                'name={self._name!r}, '
+                'address={self._address!r}, '
+                'coordinates={self._coordinates!r})'
+                ).format(self=self)
+
