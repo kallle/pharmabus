1ede95cf33044cab6220279d790e0575741f905d
Simon Koch <simon.koch@tu-braunschweig.de>
2020-03-21 17:35:15 +0100

    added a population script to preliminary fill the database

---
+++ database/population.sql
@@ -0,0 +1,26 @@
+INSERT INTO drivers VALUES
+(1, 'Dr. Krieger'    ,  100, 38118, 'HMS Date..' , 42, 52, 10, 10, 20, 30, 10, 20, 30),
+(2, 'Mellaroy Archer',  100, 38118, 'Minibar'    , 42, 50,  8, 10, 20, 30, 10, 20, 30),
+(3, 'Ray Gillette'   ,  100, 38118, 'Closet'     , 42, 48,  6, 10, 20, 30, 10, 20, 30);
+
+
+INSERT INTO pharmacies VALUES
+(1, 'International Secret Intelligence Service HQ', 38118, 'Wherever', 42, 50, 10),
+(2, 'Organisation of Democratic Intelligence Networks HQ', 38118, 'Wherever', 42, 50, 10);
+
+
+INSERT INTO meds VALUES
+(1, 1234, 'Rotkäppchen', 'Sekt (Trocken)', '750ml', 30, 20 ,20, 1, 0),
+(2, 2234, 'Mumm', 'Sekt (Trocken)', '750ml', 30, 20 ,20, 1, 0),
+(3, 3234, 'Beluga Transatlantik', 'Wodka', '500ml', 20, 10 ,10, 1, 1),
+(4, 4234, 'Billy Boys', 'Kondome', '30 Stk', 15, 5 , 10, 0, 1),
+(5, 5234, 'Aspirin', 'Acetylsalicylsäure', '20 Stk', 5, 5, 5, 0, 0);
+
+
+INSERT INTO pharmacy_stores VALUES
+(1, 1, 1),
+(1, 2, 2),
+(1, 3, 3),
+(2, 1, 4),
+(2, 5, 5);
+
