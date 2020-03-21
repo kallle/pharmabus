dcfd2e97f033e6fc7de3977e882cf422d0a45847
David Klein <david.klein@tu-braunschweig.de>
2020-03-21 16:22:52 +0100

    Added order model.

---
+++ models/order.py
@@ -0,0 +1,23 @@
+class Order:
+    def __init__(self, id, patient, medications):
+        self._id = id
+        self._patient = patient
+        self._medications = medications
+
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def patient(self):
+        return self._patient
+
+    @property
+    def medications(self):
+        return self._medications
+
+    def __repr__(self):
+        return ('Order(id={self._id!r}, '
+                'patient={self._patient!r}, '
+                'medications={self._medications!r}, '
+                ).format(self=self)
