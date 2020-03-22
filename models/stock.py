e6357ac8f0cd25a7a013b92bb3719a9a2ae242d4
Simon Koch <simon.koch@tu-braunschweig.de>
2020-03-22 11:17:17 +0100

    added a basic stock handling class

---
+++ models/stock.py
@@ -0,0 +1,10 @@
+class stock:
+
+    def __init__(self, med_amount_list):
+        self._stock = dict()
+        for elem in med_amount_list:
+            self._stock[elem[0]] = elem[1]
+
+        @property
+        def stock(self):
+            return self._stock
