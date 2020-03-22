752b63377a7f4e8693d2f328d788b195a3915f88
Simon Koch <simon.koch@tu-braunschweig.de>
2020-03-22 22:47:27 +0100

    cleanup, merged database foder into data

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sqlite3 db.sqlite < database/schema.sql
-sqlite3 db.sqlite < database/population.sql
+sqlite3 db.sqlite < data/schema.sql
+sqlite3 db.sqlite < data/population.sql
 export FLASK_ENV=development
 export FLASK_APP=main.py
 exec flask run
