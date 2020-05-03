df84eca6bc4b7edbc8793aa16ae07f0142533d3c
Simon Koch <simon.koch@tu-braunschweig.de>
2020-05-03 15:08:52 +0200

    made sure that the properties can only be get (not supposed to be set)

--- models/order.py
+++ models/order.py
@@ -1,7 +1,6 @@
 class Order:
-    def __init__(self, id, title, status, prescription, patient, doctor, pharmacy):
+    def __init__(self, id, status, prescription, patient, doctor, pharmacy):
         self._id = id
-        self._title = title
         self._status = status
         self._prescription = prescription
         self._patient = patient
@@ -13,28 +12,56 @@ class Order:
         return self._id
 
     @property
+    def id(self,value):
+        raise Exception("this is a read only object")
+
+    @property
     def title(self):
         return self._title
+
+    @property
+    def title(self,value):
+        raise Exception("this is a read only object")
 
     @property
     def status(self):
         return self._status
 
     @property
+    def status(self,value):
+        raise Exception("this is a read only object")
+
+    @property
     def prescription(self):
         return self._prescription
+
+    @property
+    def prescription(self,value):
+        raise Exception("this is a read only object")
 
     @property
     def patient(self):
         return self._patient
 
     @property
+    def patient(self,value):
+        raise Exception("this is a read only object")
+
+    @property
     def doctor(self):
         return self._doctor
 
     @property
+    def doctor(self,value):
+        raise Exception("this is a read only object")
+
+    @property
     def pharmacy(self):
         return self._pharmacy
+
+    @property
+    def pharmacy(self,value):
+        raise Exception("this is a read only object")
 
     def __repr__(self):
         return ('Order(id={self._id!r}, '
