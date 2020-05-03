1dfb1ef08406af350bfa62763d541fbfa03d535f
Simon Koch <simon.koch@tu-braunschweig.de>
2020-05-03 15:59:51 +0200

    added doctor class

---
+++ models/doctor.py
@@ -0,0 +1,7 @@
+from user import User
+
+class Doctor (User):
+
+    def __init__(self, id, email, surname, familyname, plz, street, streetno, tel, longitude, latitude):
+        super().__init__(id, email, surname, familyname, plz, street, streetno, tel, longitude, latitude)
+
