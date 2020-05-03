09f586e51589d9dcf0f0e08cfe273cbf9f4a4e02
Simon Koch <simon.koch@tu-braunschweig.de>
2020-05-03 13:21:52 +0200

    a stop has a type and relates to a order (i.e., pick up recipe, pick up med, drop off)

--- models/stop.py
+++ models/stop.py
@@ -1,34 +1,18 @@
 class Stop:
-    def __init__(self, kind, where, meds):
+    def __init__(self, kind, related_order):
         self._kind = kind
-        self._meds = meds
-        self._where = where
+        self._related_order = related_order
 
     @property
     def kind(self):
         return self._kind
 
     @property
-    def meds(self):
+    def related_order(self):
         return self._meds
 
-    @property
-    def where(self):
-        return self._where
-
     def __eq__(self, other):
-        if self.where != other.where:
+        if self.related_order != other.related_order:
             return False
-        elif self.kind != other.kind:
-            return False
-        elif self.meds != other.meds:
-            for elem in self.meds:
-                found = False
-                for other_elem in other.meds:
-                    if elem == other_elem:
-                        found = True
-                        break
-                if not found:
-                    return False
         else:
-            return True
+            return self.kind == other.kind
