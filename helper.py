1a577867064ebd003794c6a0ee308c1d226ffad1
Simon Koch <simon.koch@tu-braunschweig.de>
2020-05-03 17:03:55 +0200

    adapted the function to be not rely on deprecated code now returns rempy route

--- helper.py
+++ helper.py
@@ -1,10 +1,8 @@
 import codecs
 import csv
 
-from data.dao import get_driver_by_id, get_pharmacy_by_id, get_patient_by_id, get_med
 from models.dimensions import Dimensions
 from models.medication import Medication
-from models.post import Post
 from models.route import Route
 from models.stop import Stop
 
@@ -49,25 +47,26 @@ def allowed_file(filename):
 
 # Generates a static route to test the rendering
 def make_fake_route(cursor):
-    driver1 = get_driver_by_id(cursor, 3)
-    apo1 = get_pharmacy_by_id(cursor, 1)
-    apo2 = get_pharmacy_by_id(cursor, 2)
-    pat1 = get_patient_by_id(cursor, 1)
-    pat2 = get_patient_by_id(cursor, 2)
-    pat3 = get_patient_by_id(cursor, 3)
-    med1 = get_med("ACC 200 Brausetabletten", "Hexal", cursor)
-    med2 = get_med("Tamiflu 45 mg", "Roche Pharma", cursor)
-    med3 = get_med("ACC 200 Brausetabletten", "Hexal", cursor)
-    stops1 = []
-    stops1.append(Stop("Start", driver1, None))
-    pickup1 = [Post(med1, 4), Post(med2, 3)]
-    stops1.append(Stop("Einladen", apo1, pickup1))
-    drop1 = [Post(med1, 2), Post(med2, 1)]
-    stops1.append(Stop("Ausliefern", pat1, drop1))
-    pickup2 = [Post(med3, 3)]
-    stops1.append(Stop("Einladen", apo2, pickup2))
-    drop2 = [Post(med3, 3), Post(med2, 1)]
-    stops1.append(Stop("Ausliefern", pat2, drop2))
-    drop3 = [Post(med1, 2), Post(med2, 1)]
-    stops1.append(Stop("Ausliefern", pat3, drop3))
-    return Route(driver1, stops1)
+    return Route([])
+    # driver1 = get_driver_by_id(cursor, 3)
+    # apo1 = get_pharmacy_by_id(cursor, 1)
+    # apo2 = get_pharmacy_by_id(cursor, 2)
+    # pat1 = get_patient_by_id(cursor, 1)
+    # pat2 = get_patient_by_id(cursor, 2)
+    # pat3 = get_patient_by_id(cursor, 3)
+    # med1 = get_med("ACC 200 Brausetabletten", "Hexal", cursor)
+    # med2 = get_med("Tamiflu 45 mg", "Roche Pharma", cursor)
+    # med3 = get_med("ACC 200 Brausetabletten", "Hexal", cursor)
+    # stops1 = []
+    # stops1.append(Stop("Start", driver1, None))
+    # pickup1 = [Post(med1, 4), Post(med2, 3)]
+    # stops1.append(Stop("Einladen", apo1, pickup1))
+    # drop1 = [Post(med1, 2), Post(med2, 1)]
+    # stops1.append(Stop("Ausliefern", pat1, drop1))
+    # pickup2 = [Post(med3, 3)]
+    # stops1.append(Stop("Einladen", apo2, pickup2))
+    # drop2 = [Post(med3, 3), Post(med2, 1)]
+    # stops1.append(Stop("Ausliefern", pat2, drop2))
+    # drop3 = [Post(med1, 2), Post(med2, 1)]
+    # stops1.append(Stop("Ausliefern", pat3, drop3))
+    # return Route(driver1, stops1)
