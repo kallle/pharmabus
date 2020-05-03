5da663403c66bedde53adaa8d3b8c2d3483cc988
Simon Koch <simon.koch@tu-braunschweig.de>
2020-05-03 13:23:13 +0200

    a driver is a type of user now with a max range and current route

--- models/driver.py
+++ models/driver.py
@@ -1,63 +1,24 @@
 class Driver:
-    def __init__(self, id, name, range, address, coordinates, cooler_dimensions, storage_dimensions):
-        self._id = id
-        self._name = name
-        self._range = range
-        self._address = address
-        self._coordinates = coordinates
-        self._cooler_dimensions = cooler_dimensions
-        self._storage_dimensions = storage_dimensions
+    def __init__(self, id, pwd, surname, familyname, plz, street, streetno, longitude, latitude, max_range, current_route):
+        super().__init__(id, pwd, surname, familyname, plz, street, streetno, longitude, latitude)
+        self._max_range = max_range
+        self._current_route = current_route
 
     @property
-    def id(self):
-        return self._id
-
-    @property
-    def name(self):
-        return self._name
-
-    @property
-    def range(self):
-        return self._range
-
-    @property
-    def address(self):
-        return self._address
-
-    @property
-    def coordinates(self):
-        return self._coordinates
-
-    @property
-    def cooler_dimensions(self):
-        return self._cooler_dimensions
-
-    @property
-    def storage_dimensions(self):
-        return self._storage_dimensions
+    def max_range(self):
+        return self._max_range
 
     def __repr__(self):
         return ('Driver(id={self._id!r}, '
                 'name={self._name!r}, '
-                'range={self._range!r}, '
+                'range={self._max_range!r}, '
                 'address={self._address!r}, '
                 'coordinates={self._coordinates!r})'
-                'cooler_dimensions={self._cooler_dimensions!r})'
-                'storage_dimensions={self._storage_dimensions!r})'
                 ).format(self=self)
 
+
     def with_id(self, id):
+        raise Exception("Depricated?")
         return Driver(id, self._name, self._range, self._address, self._coordinates, self._cooler_dimensions, self._storage_dimensions)
-
-    def __eq__(self, other):
-        return self.id == other.id
-
-    def __hash__(self):
-        return hash(self.id)
-
-    def geq(self, other):
-        self_volume = self.cooler_dimensions.volume() + self.storage_dimensions.volume()
-        other_volume = other.cooler_dimensions.volume() + other.storage_dimensions.volume()
-        return self_volume >= other_volume
 
 
