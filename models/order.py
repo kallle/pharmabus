9d02c868654cca470dccfeac44096064119f6504
Simon Koch <simon.koch@tu-braunschweig.de>
2020-05-03 13:22:51 +0200

    a order contains more meta data now

--- models/order.py
+++ models/order.py
@@ -1,25 +1,40 @@
 class Order:
-    def __init__(self, id, patient, medications, status):
+    def __init__(self, id, title, status, prescription, patient, doctor, pharmacy):
         self._id = id
-        self._patient = patient
-        self._medications = medications
+        self._title = title
         self._status = status
+        self._prescription = prescription
+        self._patient = patient
+        self._doctor = doctor
+        self._pharmacy = pharmacy
 
     @property
     def id(self):
         return self._id
 
     @property
+    def title(self):
+        return self._title
+
+    @property
     def status(self):
         return self._status
+
+    @property
+    def prescription(self):
+        return self._prescription
 
     @property
     def patient(self):
         return self._patient
 
     @property
-    def medications(self):
-        return self._medications
+    def doctor(self):
+        return self._doctor
+
+    @property
+    def pharmacy(self):
+        return self._pharmacy
 
     def __repr__(self):
         return ('Order(id={self._id!r}, '
