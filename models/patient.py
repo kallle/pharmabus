af73caeaec6c1ad451487b4cd580480ad4d0ed4f
Simon Koch <simon.koch@tu-braunschweig.de>
2020-05-03 13:22:44 +0200

    a patient is now a type of user

--- models/patient.py
+++ models/patient.py
@@ -1,39 +1,4 @@
-# TODO: keep list of orders in here?
-class Patient:
-    def __init__(self, id, name, address, coordinates):
-        self._id = id
-        self._name = name
-        self._address = address
-        self._coordinates = coordinates
+from user import User
 
-    @property
-    def id(self):
-        return self._id
-
-    @property
-    def name(self):
-        return self._name
-
-    @property
-    def address(self):
-        return self._address
-
-    @property
-    def coordinates(self):
-        return self._coordinates
-
-    def __repr__(self):
-        return ('Patient(id={self._id!r}, '
-                'name={self._name!r}, '
-                'address={self._address!r}, '
-                'coordinates={self._coordinates!r})'
-                ).format(self=self)
-
-    def __eq__(self, other):
-        return self.id == other.id
-
-    def __hash__(self):
-        return hash(self.id)
-
-    def geq(self, other):
-        return self.coordinates.geq(other.coordinates)
+class Patient(User):
+    pass
