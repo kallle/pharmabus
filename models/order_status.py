297339b1b5fcd785b47235a1071a36d3f9c17bba
Simon Koch <simon.koch@tu-braunschweig.de>
2020-05-05 19:18:02 +0200

    added toString and fromString functions

--- models/order_status.py
+++ models/order_status.py
@@ -5,4 +5,33 @@ class OrderStatus(Enum):
     AT_PATIENT = "at_patient"
     AT_PHARMACY = "at_pharmacy"
     AT_DRIVER = "at_driver"
+    AT_DOCTOR = "at_doctor"
     DELIVERED = "delivered"
+
+    def toString(enum_value):
+        if enum_value == OrderStatus.AT_PATIENT:
+            return "at_patient"
+        elif enum_value == OrderStatus.AT_DOCTOR:
+            return "at_doctor"
+        elif enum_value == OrderStatus.AT_PHARMACY:
+            return "at_pharamcy"
+        elif enum_value == OrderStatus.AT_DRIVER:
+            return "at_driver"
+        elif enum_value == OrderStatus.DELIVERED:
+            return "delivered"
+        else:
+            raise Exception("unsuppored order status enum value {}".format(enum_value))
+
+    def fromString(enum):
+        if enum == "at_patient":
+            return OrderStatus.AT_PATIENT
+        elif enum == "at_doctor":
+            return OrderStatus.AT_DOCTOR
+        elif enum == "at_pharmacy":
+            return OrderStatus.AT_PHARMACY
+        elif enum == "at_driver":
+            return OrderStatus.AT_DRIVER
+        elif enum == "delivered":
+            return OrderStatus.DELIVERED
+        else:
+            raise Exception("unsupported string for order status {}".format(enum))
--- models/prescription_status.py
+++ models/prescription_status.py
@@ -5,6 +5,22 @@ class PrescriptionStatus(Enum):
     PRESENT_AT_DOCTOR = "PRESENT_AT_DOCTOR"
     PRESENT_AT_PATIENT = "PRESENT_AT_PATIENT"
 
+    def toString(enumValue):
+        if enumValue == PrescriptionStatus.PRESENT_AT_DOCTOR:
+            return "PRESENT_AT_DOCTOR"
+        elif enumValue == PrescriptionStatus.PRESENT_AT_PATIENT:
+            return "PRESENT_AT_PATIENT"
+        else:
+            raise Exception("unsupporeted prescription status value {}".format(enumValue))
+
+    def fromString(enum):
+        if enum == "PRESENT_AT_DOCTOR":
+            return PrescriptionStatus.PRESENT_AT_DOCTOR
+        elif enum == "PRESENT_AT_PATIENT":
+            return PrescriptionStatus.PRESENT_AT_PATIENT
+        else:
+            raise Exception("unsopported string for prescription status {}".format(enum))
+
 
 class InvalidPrescriptionStatus(Exception):
 
